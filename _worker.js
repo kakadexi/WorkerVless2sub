11154998b673305a75e3eca531c5e86e7a372784
kakadexi <157953405+kakadexi@users.noreply.github.com>
2024-05-26 07:28:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,6 +11,7 @@ let addresses = [
 // 设置优选地址api接口
 let addressesapi = [
 	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt',
+	'https://raw.githubusercontent.com/kakadexi/ip/main/ip.txt',
 	'https://addressesapi.090227.xyz/CloudFlareYes',
 	'https://addressesapi.090227.xyz/ip.164746.xyz',
 	'https://ipdb.api.030101.xyz/?type=bestproxy&country=true',
